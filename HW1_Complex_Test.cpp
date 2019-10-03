7a6fa5e4eb7c2aa0bb3b3c10179698e28b8dfa5b
oleneyl <39217532+oleneyl@users.noreply.github.com>
2019-10-03 12:11:32 +0900

    Assingment2

--- HW1_Complex_Test.cpp
+++ HW1_Complex_Test.cpp
@@ -1,4 +1,69 @@
 #include "HW1_Complex.h"
+#include "math.h"
+
+template <class T>
+Complex<T>::Complex(T a, T b){
+	re = a;
+	im = b;
+}
+
+template <class T>
+Complex<T>::Complex(){
+	re = (T)0;
+	im = (T)0;
+}
+
+template <class T>
+Complex<T> Complex<T>::operator+(const Complex& arg){
+	return Complex(re + arg.re, im + arg.im);
+}
+
+template <class T>
+Complex<T> Complex<T>::operator-(const Complex& arg){
+	return Complex(re - arg.re, im - arg.im);
+}
+
+template <class T>
+Complex<T> Complex<T>::operator*(const Complex& arg){
+	return Complex(re * arg.re - im * arg.im, re * arg.im + im * arg.re);
+}
+
+template <class T>
+Complex<T> Complex<T>::operator~() const {
+	return Complex(re, (T)(-1 * im));
+}
+
+template <class U>
+ostream& operator<< (ostream& os, const Complex<U>& cplx){
+	os << '(' << cplx.re;
+	if(cplx.im >= 0){
+		os << " + " << cplx.im;
+	}else{
+		os << " - " << (U)(-1 * cplx.im);
+	}
+	os << "i)" << endl;
+}
+
+void solveQuadratic(Complex<double>& x1, Complex<double>& x2, int a, int b, int c){
+	int determinant = (b * b - 4 * a * c);
+	double firstArg = (double)(b) * -1.0 / (double)(a) / 2.0;
+	double secondArg;
+	if(determinant >= 0){
+		secondArg = sqrt((double)(b * b - 4 * a * c)) / (double)(a) / 2.0;
+	}else{
+		secondArg = sqrt((double)(b * b - 4 * a * c) * -1.0) / (double)(a) / 2.0;
+	}
+	if(determinant > 0){
+		x1 = Complex<double>(firstArg + secondArg, 0.0);
+		x2 = Complex<double>(firstArg - secondArg, 0.0);
+	}else if(determinant == 0){
+		x1 = Complex<double>(firstArg, 0.0);
+		x2 = Complex<double>(firstArg, 0.0);
+	}else{
+		x1 = Complex<double>(firstArg, secondArg);
+		x2 = Complex<double>(firstArg, secondArg * -1);
+	}
+}
 
 int main() {
 	Complex<int> a;
